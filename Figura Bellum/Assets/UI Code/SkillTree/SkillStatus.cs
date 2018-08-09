fc46bd21eb01c644795ed9cc8a2befd3deba81fd
Lenyeto <patg.lenyeto@gmail.com>
2018-08-09 17:09:09 -0400

    Added some base stuff for skill tree.
    Not done yet.

---
+++ Figura Bellum/Assets/UI Code/SkillTree/SkillStatus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillStatus : MonoBehaviour {
+    public RawImage mRawImage;
+
+    public Texture2D mLocked;
+    public Texture2D mUnlocked;
+    public Texture2D mObtained;
+
+    /// <summary>
+    /// Changes the color behind the skill.
+    /// </summary>
+    /// <param name="i">0 is locked, 1 is able to be unlocked, 2 is when the skill is obtained.</param>
+    public void ChangeStatus(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                mRawImage.texture = mLocked;
+                break;
+            case 1:
+                mRawImage.texture = mUnlocked;
+                break;
+            case 2:
+                mRawImage.texture = mObtained;
+                break;
+        }
+    }
+}
---
+++ Figura Bellum/Assets/UI Code/SkillTree/SkillTreeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DrawLine(new Vector3(0, 0, 0), new Vector3(10, 10, 10), Color.red);
+	}
+
+    void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.SetColors(color, color);
+        lr.SetWidth(0.1f, 0.1f);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
+    }
+}
