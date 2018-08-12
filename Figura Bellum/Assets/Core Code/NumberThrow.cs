53eef7b70c22ac8caca608e321fb3f3df97f7a44
Lenyeto <patg.lenyeto@gmail.com>
2018-08-12 09:54:31 -0400

    Add Number Throw for color in numbers.

--- Figura Bellum/Assets/Core Code/NumberThrow.cs
+++ Figura Bellum/Assets/Core Code/NumberThrow.cs
@@ -34,6 +34,16 @@ public class NumberThrow : MonoBehaviour {
     //Used for crit damage at one point.
     public void ThrowNumberColored(int i, Color color)
     {
+        //DO NOT MAKE THIS A CHILD, WHEN AN ENEMY DIES, THE NUMBER SHOULD STILL BE THERE.
+        GameObject go = Instantiate(NumberHolderPrefab, gameObject.transform.position, new Quaternion());
+        go.transform.position = new Vector3(go.transform.position.x, go.transform.position.y, NumberHolderPrefab.transform.position.z);
 
+
+        go.GetComponent<Rigidbody2D>().AddForce(new Vector2((Random.value / 2), (Random.value / 2) + 0.5f) * 200);
+
+        go.GetComponent<TextMesh>().text = i.ToString();
+        go.GetComponent<TextMesh>().color = color;
+
+        Destroy(go, 5);
     }
 }
